e98c8d8c894815c33273a653a69ac798b0fcdc81
danh tran <tldanh1993@gmail.com>
2021-12-14 08:41:35 +0700

    done layout demo phone list

--- src/Props/DanhSachSanPham.js
+++ src/Props/DanhSachSanPham.js
@@ -2,22 +2,48 @@ import React, { Component } from 'react';
 import SanPham from './SanPham';
 import dataPhone from '../Data/dataPhone.json';
 // import dataPhone_bug from '../Data/dataPhone_bug.json';
+// BUG dính kí tự lạ nhưng check ko ra -> config webpack ?
 
 export default class DanhSachSanPham extends Component {
+  // Bước tạo biến mới gán lại có vẻ ko cần lắm
+  // Gọi trực tiếp dataPhone ra thao tác vẫn ok
   mangSanPham = dataPhone;
 
   state = {
     sanPhamChiTiet: this.mangSanPham[0],
   };
 
+  renderSanPham = () => {
+    return this.mangSanPham.map((sanPham, index) => {
+      return (
+        <div className="col-4" key={index}>
+          <SanPham sanPhamProps={sanPham} />
+        </div>
+      );
+    });
+  };
+
   render() {
-    let { manHinh, heDieuHanh, cameraTruoc, cameraSau, ram, rom } =
-      this.state.sanPhamChiTiet;
+    let {
+      manHinh,
+      heDieuHanh,
+      cameraTruoc,
+      cameraSau,
+      ram,
+      rom,
+      tenSP,
+      hinhAnh,
+    } = this.state.sanPhamChiTiet;
+
     return (
       <div className="container">
         <h3 className="display-4 text-center">DANH SÁCH SẢN PHẨM</h3>
+        <div className="row">{this.renderSanPham()}</div>
         <div className="row">
-          <div className="col-4">this.renderSanPham()</div>
+          <div className="col-4">
+            <h3>{tenSP}</h3>
+            <img style={{ width: '100%' }} src={hinhAnh} alt={tenSP} />
+          </div>
           <div className="col-8">
             <h3>Thông số kỹ thuật</h3>
             <table className="table">
--- src/Props/SanPham.js
+++ src/Props/SanPham.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 
 export default class SanPham extends Component {
   render() {
+    let { sanPhamProps } = this.props;
+    // destructering hơi sida, có thể tách xuống sâu hơn nữa
+    // demo cách mới thử
+    // TODO fix thử css image
+    
     return (
       <div className="card text-left text-center">
+        <img
+          style={{ width: 250, height: 250, marginLeft: 50 }}
+          className="card-img-top"
+          src={sanPhamProps.hinhAnh}
+          alt={sanPhamProps.tenSP}
+        />
         <div className="card-body">
-          <h4 className="card-title">sanPhamProps.tenSP</h4>
-          <p className="card-text">sanPhamProps.giaBan.toLocaleString()</p>
+          <h4 className="card-title">{sanPhamProps.tenSP}</h4>
+          <p className="card-text">{sanPhamProps.giaBan.toLocaleString()}</p>
           <button onClick={() => {}} className="btn btn-success">
             Xem chi tiết
           </button>
