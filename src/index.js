852cf6d4f5e96eeab1020656de86bca5bb1b8491
danh tran <tldanh1993@gmail.com>
2021-12-15 09:42:36 +0700

    setup base redux, redux chorme devtools

--- src/index.js
+++ src/index.js
@@ -7,11 +7,40 @@ import reportWebVitals from './reportWebVitals';
 // Nếu gọi trực tiếp Prouct demo và console log prop thì có TH bị log 2 lần
 // Fix bằng cách bỏ tag StricMode bọc ngoài
 
+// setup redux
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './redux/rootReducer';
+
+const store = createStore(rootReducer);
+
+/**
+ * https://github.com/zalmoxisus/redux-devtools-extension
+ * In case ESLint is configured to not allow using the underscore dangle, wrap it like so:
+ * Tạm thời chưa dùng liền
+ * ext của chorme
+ * chạy trên ff / các trình duyệt khác sẽ bị lỗi
+ */
+
+// const store = createStore(
+//   rootReducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
 ReactDOM.render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
-  <App />,
+  /**
+   * no strict mode
+   */
+  // <App />,
+  /**
+   * Using redux
+   */
+  <Provider store={store}>
+    <App />
+  </Provider>,
 
   document.getElementById('root')
 );
