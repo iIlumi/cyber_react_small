a0c0838f36166e6be630d02b5dcbc14c6a6da3bb
danh tran <tldanh1993@gmail.com>
2021-12-18 02:19:21 +0700

    Provider-Context __Consumer connectStateToProps

--- src/ExContextModelList/ModelItem.js
+++ src/ExContextModelList/ModelItem.js
@@ -1,31 +1,45 @@
 import React, { Component } from 'react';
+import { ExModelListContext } from '../ContextDemo/_Context/ExModelListContext';
 
 export default class ModelItem extends Component {
   render() {
+    // Đặc điểm của Context-Provider là ko cần connectStateToProps và mapDispatch
+    // Tiện code nhưng lại phải wrap Consummer bù
+    // Cần kết hợp PureComponent vì tính chất của setState sẽ render bất chấp mọi component thường
+    let { img, name, age, id, like } = this.props.modelItem ;
     return (
-      <div>
-        <div className="card text-white bg-default text-dark">
-          <img
-            className="card-img-top"
-            src="./img/btContextModel/model1.png"
-            alt="./img/btContextModel/model1.png"
-          />
-          <div className="card-body">
-            <h4 className="card-title">Họ Tên: Default</h4>
-            <p className="card-text">Tuổi: 18</p>
-            <button
-              style={{
-                background: 'none',
-                border: '1px solid red',
-                borderRadius: '5px',
-              }}
-              className="card-text"
-            >
-              8 <i style={{ color: 'red' }} className="fa fa-heart"></i>
-            </button>
-          </div>
-        </div>
-      </div>
+      <ExModelListContext.Consumer>
+        {(value) => {
+          // Value là reducer local bao gồm state và các method trong Provider
+          console.log('value:', value)
+          return (
+            <div className="card text-white bg-default text-dark">
+              <img
+                className="card-img-top"
+                src = {img}
+                alt = {img}
+              />
+              <div className="card-body">
+                <h4 className="card-title">Họ Tên: {name}</h4>
+                <p className="card-text">Tuổi: {age}</p>
+                <button
+                  style={{
+                    background: 'none',
+                    border: '1px solid red',
+                    borderRadius: '5px',
+                  }}
+                  className="card-text"
+                  onClick={() => {
+                    // value.setActiveModel(modelItem.id);
+                  }}
+                >
+                  {like} <i style={{ color: 'red' }} className="fa fa-heart"></i>
+                </button>
+              </div>
+            </div>
+          );
+        }}
+      </ExModelListContext.Consumer>
     );
   }
 }
--- src/ExContextModelList/ModelList.js
+++ src/ExContextModelList/ModelList.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import ModelItem from './ModelItem';
+import { ExModelListContext } from '../ContextDemo/_Context/ExModelListContext';
 
 export default class ModelList extends Component {
   render() {
+    console.log('modelList loaded');
     return (
       <div>
         <h3>Danh sách idol</h3>
-        <div className="row">
-          <div className="col-3">
-            <ModelItem />
-          </div>
-          <div className="col-3">
-            <ModelItem />
-          </div>
-          <div className="col-3">
-            <ModelItem />
-          </div>
-          <div className="col-3">
-            <ModelItem />
-          </div>
-        </div>
+
+        <ExModelListContext.Consumer>
+          {(value) => {
+            console.log(value);
+            return (
+              <div className="row">
+                {value.modelListState.map((model, index) => {
+                  return (
+                    <div className="col-3" key={index}>
+                      <ModelItem modelItem={model} />
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          }}
+        </ExModelListContext.Consumer>
       </div>
     );
   }
