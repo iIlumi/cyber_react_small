7edb5be0e2d1ae450bd802ddf28b4f4a43b6985c
danh tran <tldanh1993@gmail.com>
2021-12-19 09:51:24 +0700

    complete form layout

--- src/FormValidation/UserProfile/UserProfile.js
+++ src/FormValidation/UserProfile/UserProfile.js
@@ -16,32 +16,86 @@ export default class UserProfile extends Component {
   render() {
     return (
       // Return về orm, ko phải div
-      <form>
-        <Helmet>
-          <link rel="stylesheet" href="/cssHelmet/UserProfile.css" />
-        </Helmet>
-        <h1>User Profile</h1>
-        <div className="container">
+      <div
+        className="container-fluid"
+        style={{
+          backgroundColor: '#EEEEEE',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <form
+          style={{
+            fontSize:
+              'font-family: "Google Sans", "Noto Sans Myanmar UI", arial, sans-serif',
+            width: 600,
+          }}
+          className=" bg-white p-5 m-5"
+        >
+          <Helmet>
+            <link rel="stylesheet" href="/cssHelmet/UserProfile.css" />
+          </Helmet>
+          <h1 className="text-center mt-0 mb-5">User Profile</h1>
           <div className="row">
             <div className="col-6">
               <div className="group">
-                <input type="text" required />
+                <input type="text" name="firstName" required />
                 <span className="highlight" />
                 <span className="bar" />
-                <label>Name</label>
+                <label>firstName</label>
               </div>
             </div>
             <div className="col-6">
               <div className="group">
-                <input type="text" required />
+                <input type="text" name="lastName" required />
                 <span className="highlight" />
                 <span className="bar" />
-                <label>Name</label>
+                <label>lastName</label>
               </div>
             </div>
+            <div className="col-12">
+              <div className="group">
+                <input type="text" name="userName" required />
+                <span className="highlight" />
+                <span className="bar" />
+                <label>userName</label>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="group">
+                <input type="text" name="email" required />
+                <span className="highlight" />
+                <span className="bar" />
+                <label>email</label>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="group">
+                <input name="passWord" type="password" required />
+                <span className="highlight" />
+                <span className="bar" />
+                <label>password</label>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="group">
+                <input name="passWordConfirm" type="password" required />
+                <span className="highlight" />
+                <span className="bar" />
+                <label>passwordConfirm</label>
+              </div>
+            </div>
+            <div className="col-12">
+              <button
+                className="btn text-white bg-dark w-100"
+                style={{ fontSize: 25 }}
+              >
+                Submit
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     );
   }
 }
